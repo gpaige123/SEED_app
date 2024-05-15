06c20f7a034d8f65fec7cecb7beb21131f69ee32
Geremy Paige <gpaige@mines.edu>
2024-05-14 22:18:51 -0400

    Initial Commit

---
+++ SEEDCrypto/SEEDCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEEDCryptoApp.swift
+//  SEEDCrypto
+//
+//  Created by Geremy Paige on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SEEDCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
